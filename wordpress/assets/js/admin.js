5368564a63a37a1ccc64d68d299085992fc8c64d
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-21 15:59:02 +0000

    Add: wordpress/assets/js/admin.js
    ✨ Auto-committed on 2026-05-21 15:59:02

---
+++ wordpress/assets/js/admin.js
@@ -0,0 +1,8 @@
+(function ($) {
+    'use strict';
+
+    $(function () {
+        // Initialize WordPress color picker on color fields
+        $('.aparsoft-color-field').wpColorPicker();
+    });
+})(jQuery);
